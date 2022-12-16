114b1c8151a993d8ba87d1c3241916c2eab6e7ad
sterlingh78 <sterlingh78@yahoo.com>
2022-12-15 18:38:25 -0700

    railwayconfig

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-import fetch from "node-fetch"
+import { fetch } from "node-fetch"
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
